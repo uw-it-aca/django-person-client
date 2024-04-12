b8a0a05e61a79153ee82e1ad32095680c82f3593
Jim Laney <jlaney@uw.edu>
2024-04-12 09:24:50 -0700

    add tests for major setters

---
+++ uw_person_client/tests/test_major.py
@@ -0,0 +1,31 @@
+# Copyright 2024 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from uw_person_client.tests import ModelTest
+from uw_person_client.models import Person, Major
+
+
+class MajorTest(ModelTest):
+    def test_get_set_majors(self):
+        p = Person.objects.get_person_by_uwnetid(
+            'javerage', include_student=True)
+        self.assertEqual(len(p.student.majors), 2)
+
+        p.student.majors.append(Major())
+        self.assertEqual(len(p.student.majors), 3)
+
+        new_majors = [Major(), Major(), Major()]
+        p.student.majors = new_majors
+        self.assertEqual(len(p.student.majors), 3)
+
+    def test_get_set_pending_majors(self):
+        p = Person.objects.get_person_by_uwnetid(
+            'javerage', include_student=True)
+        self.assertEqual(len(p.student.pending_majors), 0)
+
+        p.student.pending_majors.append(Major())
+        self.assertEqual(len(p.student.pending_majors), 1)
+
+        new_majors = [Major(), Major()]
+        p.student.pending_majors = new_majors
+        self.assertEqual(len(p.student.majors), 2)
